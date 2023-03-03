a893fa2246f14594644e8074f4b951fad2c5b89f
katariya dharmesh <katariyadharmesh658@gmail.com>
2023-03-03 10:26:49 +0530

    screen 14 work start

---
+++ lib/screen14/screen14page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClothesPage extends StatefulWidget{
+  const ClothesPage({Key? key}) : super(key: key);
+
+  @override
+  State<ClothesPage> createState() => _ClothesPageState();
+}
+
+class _ClothesPageState extends State<ClothesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
